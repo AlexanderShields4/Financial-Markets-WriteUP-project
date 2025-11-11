09f78414a9b2e4149a5063a74e154875ef8b389b
Alex <alshields1@davidson.edu>
2025-11-11 11:15:48 +0100

    automated update

--- newsletter_collector.py
+++ newsletter_collector.py
@@ -23,7 +23,7 @@ def main():
     load_dotenv()
     # Initialize Chrome options for headless operation
     options = Options()
-    options.add_argument("--headless")  # Run in headless mode
+    options.add_argument("--headless") 
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     
