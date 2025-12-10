f5b4d703f8252c672c0a442b5273c4f25f5a4ee6
Alex <alshields1@davidson.edu>
2025-12-10 23:01:33 +0100

    automated update

--- newsletter_collector.py
+++ newsletter_collector.py
@@ -292,7 +292,7 @@ def main():
     
     client = genai.Client(api_key=os.getenv("GOOGLE_KEY"))
     response = client.models.generate_content(
-        model="gemini-2.5-pro",
+        model="gemini-2.5-flash",
         contents=message
     ) 
     response = str(response.text)
